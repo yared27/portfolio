de00a9c571d872035a4898af959cd8cf23d4e5c6
Yared <alemayehuyared9@gmail.com>
2025-05-23 02:45:03 -0700

    fix deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './components/Hero';
 import Services from './components/services';
 import About from './components/about';
-import Contact from './components/contact';
+import Contact from './components/contact.jsx';
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-900">
